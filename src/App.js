210ae16034bc8002d0b6e755cd3845fbf604eddb
adi-42 <rajaditya2704@gmail.com>
2023-09-10 19:32:08 +0530

    setup react basics

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import About from './components/About';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Sharebook</h1>
-    </div>
+    <>
+    <Router>
+    <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/about" element={<About/>}/>
+      </Routes>
+    </Router>
+    </>
   );
 }
 
